aca0f85f84c0795e4342d0aa06c59b5a26e55f47
Marco Gonzalez <mgonzalez11@hawk.iit.edu>
2024-03-08 14:39:14 -0600

    Initial Commit

---
+++ Homework 5 1D Schrodinger Equation Calculator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Homework 5 1D Schrodinger Equation Calculator
+//
+//  Created by PHYS 440 Marco on 3/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Homework 5 1D Schrodinger Equation Calculator/Homework_5_1D_Schrodinger_Equation_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Homework_5_1D_Schrodinger_Equation_CalculatorApp.swift
+//  Homework 5 1D Schrodinger Equation Calculator
+//
+//  Created by PHYS 440 Marco on 3/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Homework_5_1D_Schrodinger_Equation_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
