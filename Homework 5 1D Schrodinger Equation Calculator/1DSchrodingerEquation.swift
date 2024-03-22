2aa234cd9ad184aa9b802ca12402158cb9b0e4d5
Marco Gonzalez <mgonzalez11@hawk.iit.edu>
2024-03-21 20:08:14 -0500

    Changes were finally made after spring break. I was out. At least I'm back stress free, for now.

---
+++ Homework 5 1D Schrodinger Equation Calculator/1DSchrodingerEquation.swift
@@ -0,0 +1,8 @@
+//
+//  1DSchrodingerEquation.swift
+//  Homework 5 1D Schrodinger Equation Calculator
+//
+//  Created by Marco Gonzalez on 3/21/24.
+//
+
+import Foundation
---
+++ Homework 5 1D Schrodinger Equation Calculator/RungeKutta.swift
@@ -0,0 +1,8 @@
+//
+//  RungeKutta.swift
+//  Homework 5 1D Schrodinger Equation Calculator
+//
+//  Created by Marco Gonzalez on 3/21/24.
+//
+
+import Foundation
